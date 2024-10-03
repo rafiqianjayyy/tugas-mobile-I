1091e413fc51a4c8c9ad83557dfa612fd1efa2ec
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-03 12:47:40 +0700

    Create kaca_kunci_finall_doubel_(L).dart

---
+++ kaca_kunci_finall_doubel_(L).dart
@@ -0,0 +1,10 @@
+//void main(){
+  //  var namapertama ="Muhammad";
+  //  final namaterakhir ="Rafiqi";
+  
+  // namapertama ="Muhammad Rafiqi";
+  // nama terkahir"anjayy iqi";
+    
+ //   print(namapertama);
+ //   print(namaterakhir);
+//}
